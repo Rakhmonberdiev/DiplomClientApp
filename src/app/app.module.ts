42ff1acaac4b4d1c96ef49b83d59df48d0a26253
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-16 14:52:29 +0500

    homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavbarComponent,
-        FooterComponent
+        FooterComponent,
+        HomeComponent
     ],
     imports: [
     BrowserModule,        
