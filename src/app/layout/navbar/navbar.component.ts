a42fef95cae06a33c8ad4a8cca675d5d80eeee3e
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-16 19:52:24 +0500

    logout

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AccountService } from '../../_services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,10 @@ import { AccountService } from '../../_services/account.service';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  constructor(public accountService: AccountService){}
+  constructor(public accountService: AccountService, private router: Router){}
 
+  logout(){
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
 }
