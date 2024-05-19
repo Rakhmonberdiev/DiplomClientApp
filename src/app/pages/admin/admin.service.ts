0d7d67d4c5054b5e4b580b6d097bd1cb3b139b8b
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-05-19 12:25:23 +0500

    Get dists

--- src/app/pages/admin/admin.service.ts
+++ src/app/pages/admin/admin.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { RouteEn } from "../../_models/routeEn";
+import { District } from "../../_models/district";
 
 
 
@@ -18,6 +19,9 @@ export class AdminService{
     }
 
     getAllRoutes(search:string){
-        return this.http.get<RouteEn[]>(this.baseUrl+'routes/GetAll?search='+search)
+        return this.http.get<RouteEn[]>(this.baseUrl+'routes/GetAll?search='+search);
+    }
+    getAllDists(search:string){
+        return this.http.get<District[]>(this.baseUrl+'rayon/GetAll?search='+search);
     }
 }
--- src/app/pages/admin/all-districts/all-districts.component.ts
+++ src/app/pages/admin/all-districts/all-districts.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AdminService } from '../admin.service';
+import { District } from '../../../_models/district';
 
 @Component({
   selector: 'app-all-districts',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './all-districts.component.css'
 })
 export class AllDistrictsComponent {
+  search:string ='';
+  @ViewChild('search', {static:true}) searchTerm!: ElementRef;
+  dists: District[]=[];
+  constructor(private adminService:AdminService){
 
+  }
+  ngOnInit(): void {
+    this.getDists();
+  }
+
+  getDists(){
+    this.search = this.searchTerm.nativeElement.value;
+    this.adminService.getAllDists(this.search).subscribe(rs=>{
+      this.dists = rs
+    })
+  }
+
+  onSearch(){
+    this.search = this.searchTerm.nativeElement.value;
+    this.getDists();
+  }
+
+  onReset(){
+    this.searchTerm.nativeElement.value = '';
+    this.getDists();
+  }
 }
